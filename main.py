0e5ca9f4f3406bc72f98defbd1bd44bbbce524fe
Anthony Ankomah <tonyankomah1@gmail.com>
2022-07-22 14:08:14 +0000

    initial commit for Deployment

---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Count Duplicates')
+
+@st.cache
+def convert_df(df):
+   return df.to_csv().encode('utf-8')
+
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+     # Can be used wherever a "file-like" object is accepted:
+     dataframe = pd.read_excel(uploaded_file)
+     
+     
+     option = st.selectbox(
+     'Which column do you want to be checked with ?',
+        list(dataframe.columns.values))
+     
+     count = dataframe[option].value_counts()
+     
+     st.write(count);
+     csv = convert_df(count)
+     st.download_button(
+        "Press to Download Count Data",
+        csv,
+        option+"_duplicates.csv",
+        "text/csv",
+        key='download-csv'
+    )
+     
+     
+     agree = st.checkbox('Show Original Data')
+     if agree:
+        st.write(dataframe)
+        
+       
+        
